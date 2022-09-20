67a8c01905e6aefa0608a711a163e576f5dc4d9a
IJuanI <juancojcl@hotmail.com>
2022-09-20 01:35:19 -0300

    feat: add comments and dummy data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+/** 1. Buscar 5 elementos por id. El script debe estar debajo de todos estos elementos en el html. */
 const contenedorProductos = document.getElementById('contenedor-productos')
 
 const contenedorCarrito = document.getElementById('carrito-contenedor')
@@ -6,25 +8,42 @@ const botonVaciar = document.getElementById('vaciar-carrito')
 const contadorCarrito = document.getElementById('contadorCarrito')
 
 const precioTotal = document.getElementById('precioTotal')
+
+/** Creas un arreglo para los items del carrito */
 let carrito = []
 
+/** Cuando la pagina se cargue */
 document.addEventListener('DOMContentLoaded', () => {
+    /** Buscas si hay items guardados en el navegador */
     if (localStorage.getItem('carrito')) {
+        /** Y agregas los items guardados al carrito */
         carrito = JSON.parse(localStorage.getItem('carrito'))
         actualizarCarrito()
     }
 })
 
+/** Cuando se hace click en botonVaciar, borras el carrito */
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
 
-carrito.forEach((producto) => {
+/** Para cada producto de la lista
+ * Nota: los productos se crean en stock.js
+*/
+stockProductos.forEach((producto) => {
+
+    /** Borras de nuevo el carrito al apretar el boton borrar.
+     * Esto no sirve para nada, con hacerlo una vez arriba alcanza...
+     */
     botonVaciar.addEventListener('click', () => {
         carrito.length = 0
         actualizarCarrito()
     })
+
+    /** Agregas el item al html,
+     * va a ser un div con clase 'producto', con imagen, titulo, descripcion,
+     * talle, precio y un boton para agregarlo al carrito */
     const div = document.createElement('div')
     div.classList.add('producto')
     div.innerHTML = `
@@ -35,10 +54,14 @@ carrito.forEach((producto) => {
     <p class="precioProducto">Precio:$ ${producto.precio}</p>
     <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
     `
+
+    /** Los productos se agregan como hijos del elemento con id 'contenedor-producto' en el html */
     contenedorProductos.appendChild(div)
 
+    /** Buscas el boton de este producto */
     const boton = document.getElementById(`agregar${producto.id}`)
 
+    /** Agregas el producto al carrito cuando se aprieta el boton */
     boton.addEventListener('click', () => {
         agregarAlCarrito(producto.id)
     })
